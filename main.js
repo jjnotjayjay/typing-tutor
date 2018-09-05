486c8269cc90072fc066c7ade1841ac293a8e4b0
jjnotjayjay <jj@alumni.usc.edu>
2018-09-05 13:18:24 -0700

    Enhanced function that renders a character to conditionally add a special current-character class. #3

--- main.js
+++ main.js
@@ -8,13 +8,17 @@ var appState = {
 
 for (var i = 0; i < appState.phraseToType.length; i++) {
   appState.lettersToType.push({
-    letter: appState.phraseToType[i]
+    letter: appState.phraseToType[i],
+    index: i
   })
 }
 
 function renderLetter(letter) {
   var $letter = document.createElement('span')
   $letter.textContent = letter.letter
+  if (letter.index === appState.currentCharacter) {
+    $letter.classList.add('current-character')
+  }
   return $letter
 }
 
